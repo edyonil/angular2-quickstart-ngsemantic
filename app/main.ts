391e7e479630af71c23621962dd74c58efaac765
edyonil <edyonil@gmail.com>
2016-08-10 20:40:43 -0300

    Correção de versão do Angular e erro na tipagem

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './components/app.component';
 import { APP_ROUTER_PROVIDERS } from "./routes";
 import { Auth } from "./services/auth";
 
-bootstrap(<Type>AppComponent,
+bootstrap(AppComponent,
 [
     Auth,
     APP_ROUTER_PROVIDERS
